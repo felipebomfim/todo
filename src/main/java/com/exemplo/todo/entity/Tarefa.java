df040cdf2a2c461aa6e46ae7d45e0cd5b30c6f37
Felipe Bomfim <felipe.bomfim@camara.leg.br>
2025-05-20 15:11:49 -0300

    Commit inicial

---
+++ src/main/java/com/exemplo/todo/entity/Tarefa.java
@@ -0,0 +1,20 @@
+package com.exemplo.todo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private boolean concluida;
+}
